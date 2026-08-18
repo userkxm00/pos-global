1cdc52ef30230288c457d95d853dc1efaa6e07b4
userkxm00 <karim488785454ee@gmail.com>
2026-08-17 23:01:24 -0400

    feat: add Tauri application entrypoint and updater initialization

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,11 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod db;
+
+fn main() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .run(tauri::generate_context!())
+        .expect("error while running POS Global");
+}
